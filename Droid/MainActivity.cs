a4c01523b37ec9759c4f3fec018f94bff6ae97c9
Renato Ayres <ayresrenato@hotmail.com>
2017-05-25 16:46:15 +0100

    Missing android project configuration file

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,16 +1,11 @@
-﻿using System;
-
+﻿
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace coins.Droid
 {
-	[Activity(Label = "coins.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "coins.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
